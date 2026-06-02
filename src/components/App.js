81e6b099d68cca9e1f72d70f880ecb81749169a9
imhsart <nitinrana2519@gmail.com>
2026-06-02 12:16:20 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fetchData, setFetchData] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data => {
+      setFetchData(JSON.stringify(data))
+      setLoading(false)
+    })
+    .catch(error => console.log(error))
+
+  }, [])
+
+  if(loading){
+    return <div>Loading...</div>
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetched from API</h1>
+        <pre>
+          {fetchData}
+        </pre>
     </div>
   )
 }
